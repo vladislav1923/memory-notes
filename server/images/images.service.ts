e70eedee31d5be24d756d22bb2d0590756a0f4c7
kozak <vkozak@stream.ru>
2019-12-22 13:08:34 +0300

    Rework set api key

--- server/images/images.service.ts
+++ server/images/images.service.ts
@@ -10,11 +10,12 @@ export class ImagesService {
 
   constructor(configService: ConfigService) {
     this.configService = configService;
-    this.client = new GoogleImages.GoogleSearchResults(this.configService.getGoogleApiKey());
+    this.client = new GoogleImages.GoogleSearchResults();
   }
 
   public async getImagesFromGoogleSearch(query: string) {
       this.client.json({
+        api_key: this.configService.getGoogleApiKey(),
         q: query,
       }, (result) => {
         console.log(result);
